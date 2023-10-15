580a2672055065ae5d2ce5f0ef659dfc29d4e8f0
Nikita <Nikita2871@Yandex.ru>
2023-10-15 17:42:27 +0300

    add limited offer and fix JS

--- megano/shopapp/services/limited_edition_and_limited_offer.py
+++ megano/shopapp/services/limited_edition_and_limited_offer.py
@@ -1,18 +1,33 @@
-import random
+from random import choice
 
-from shopapp.models import ProductSeller
+from django.utils import timezone
+from shopapp.models import DailyOfferProduct, ProductSeller
+
+
+def get_daily_offer():
+    today = timezone.now().date()
+
+    daily_offer = DailyOfferProduct.objects.filter(selected_date=today).first()
+
+    if not daily_offer:
+        limited_edition_products = ProductSeller.objects.filter(is_limited_edition=True)
+        if limited_edition_products:
+            selected_product = choice(limited_edition_products)
+            daily_offer = DailyOfferProduct(product=selected_product, selected_date=today)
+            daily_offer.save()
+
+    return daily_offer.product if daily_offer else None
 
 
 def get_limited_edition_products():
-    # Получение товаров из ограниченного тиража
-    limited_edition_products = ProductSeller.objects.filter(is_limited_edition=True)[:16]
+    today = timezone.now().date()
+
+    daily_offer = DailyOfferProduct.objects.filter(selected_date=today).first()
+
+    limited_edition_products = ProductSeller.objects.filter(is_limited_edition=True)
+    if daily_offer:
+        limited_edition_products = limited_edition_products.exclude(id=daily_offer.product.id)
+
+    limited_edition_products = limited_edition_products[:16]
+
     return limited_edition_products
-
-
-def get_limited_offers():
-    # Получение товара ограниченного предложения
-    limited_offers_products = get_limited_edition_products()
-    if limited_offers_products:
-        return random.choice(limited_offers_products)
-    else:
-        return None
