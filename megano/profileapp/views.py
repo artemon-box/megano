374dbc98f765528829bc259561a4a0ee7008aedf
Maksim Balonin <leo@MacBook-Pro-Maksim.local>
2023-10-16 22:20:29 +0300

    add comment 4

--- megano/profileapp/views.py
+++ megano/profileapp/views.py
@@ -8,10 +8,9 @@ from django.views import View
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from config.settings import ORDER_STATUS_CHOICES
 from .forms import ProfileAvatarForm, ProfileForm
-from cart_and_orders.models import Order, StatusOrder
-from cart_and_orders.utils.get_total_price import get_total_price_delivery
-from cart_and_orders.services.cart import CartService
+from cart_and_orders.models import Order
 
 
 class AccountView(View):
@@ -27,43 +26,22 @@ class AccountView(View):
                                                     "orders": orders,
                                                     "DELIVERY_CHOICES": settings.DELIVERY_CHOICES,
                                                     "PAYMENT_CHOICES": settings.PAYMENT_CHOICES,
-                                                    "ORDER_STATUS_CHOICES": ORDER_STATUS_CHOICES, })
+                                                    "ORDER_STATUS_CHOICES": ORDER_STATUS_CHOICES,})
 
 
 class HistoryOrdersView(View):
-    template_name_id = "profileapp/historyorder_id.jinja2"
     template_name = "profileapp/historyorder.jinja2"
 
-    def get(self, request, *args, order_id=None, **kwargs):
+    def get(self, request, *args, **kwargs):
         if not request.user.is_authenticated:
             return redirect("/")
-        if order_id:
-            request.session['current_order_id'] = order_id
-            current_order_id = request.session.get('current_order_id')
-            order = Order.objects.get(id=current_order_id)
-            total_price = get_total_price_delivery(current_order_id)
-            cart = CartService()
-            product_seller = cart.get_cart(request)
-
-            context = {
-                'order': order,
-                'order_price': total_price,
-                'cart': product_seller,
-                'STATUS_ORDER': StatusOrder,
-            }
-
-            return render(request, self.template_name_id, context)
-        else:
-            user = request.user
-            orders = Order.objects.filter(user=user)
-            context = {
-                "user": user,
-                "orders": orders,
-                "DELIVERY_CHOICES": settings.DELIVERY_CHOICES,
-                "PAYMENT_CHOICES": settings.PAYMENT_CHOICES,
-                "ORDER_STATUS_CHOICES": StatusOrder.choices,
-            }
-            return render(request, self.template_name, context)
+        user = request.user
+        orders = Order.objects.filter(user=user)
+        return render(request, self.template_name, {"user": user,
+                                                    "orders": orders,
+                                                    "DELIVERY_CHOICES": settings.DELIVERY_CHOICES,
+                                                    "PAYMENT_CHOICES": settings.PAYMENT_CHOICES,
+                                                    "ORDER_STATUS_CHOICES": Order.ORDER_STATUS_CHOICES,})
 
 
 class ProfileView(View):
